02d708d48ee69781041eab7acf7c3eda87d275c4
Ricardo Cortijo <cortijougaz@gmail.com>
2026-08-12 17:05:57 -0500

    Gestión de dependencias.

--- my-service-app/src/main/java/com/ibk/app/MsHwApplication.java
+++ my-service-app/src/main/java/com/ibk/app/MsHwApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@ConfigurationPropertiesScan
-@SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "com.ibk")
+@SpringBootApplication(scanBasePackages = "com.ibk")
 public class MsHwApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsHwApplication.class, args);
--- my-service-core/src/main/java/com/ibk/core/port/in/usecase/ActualizarClienteInputPort.java
+++ my-service-core/src/main/java/com/ibk/core/port/in/usecase/ActualizarClienteInputPort.java
@@ -1,9 +1,10 @@
 package com.ibk.core.port.in.usecase;
 
 import com.ibk.core.model.Cliente;
+import com.ibk.core.model.RegisterUserCommand;
 
 import java.util.UUID;
 
 public interface ActualizarClienteInputPort {
-    Cliente actualizarCliente(HeaderRequest headers, UUID id, Cliente cliente);
+    Cliente actualizarCliente(RegisterUserCommand headers, UUID id, Cliente cliente);
 }
